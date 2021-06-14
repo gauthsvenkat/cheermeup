8f0003cf1d37d6a23673c4d2cd7e6ad75debd19a
Gautham <gauthsvenkat@gmail.com>
2021-06-14 19:09:43 +0530

    fixed content type in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     version          = '1.0.0',
     description      = 'Command line program to play random animal videos.',
     long_description = read('README.md'),
+    long_description_content_type='text/markdown',
     license_files    = ('LICENSE',),
     url              = 'https://github.com/andohuman/cheermeup',
     download_url     = 'https://github.com/andohuman/cheermeup/archive/refs/tags/v1.0.0.tar.gz',
